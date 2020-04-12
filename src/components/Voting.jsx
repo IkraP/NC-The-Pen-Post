59a5ceef9aca9342c92960c8284e86a0f98f026c
ikraP <ikrapervaiz@gmail.com>
2020-04-12 13:40:59 +0100

    vote component changed

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -6,30 +6,29 @@ import ErrorPage from "./ErrorPage";
 export default class Voting extends Component {
   state = {
     voteChange: 0,
-    err: null
+    err: null,
   };
 
-  handleClick = inc_votes => {
+  handleClick = (inc_votes) => {
     const { article_id } = this.props;
     const { comment_id } = this.props;
 
-    this.setState(currentState => {
+    this.setState((currentState) => {
       return { voteChange: currentState.voteChange + inc_votes };
     });
     if (!article_id)
       api
         .patchVotesByCommentId(comment_id, inc_votes)
-        .catch(err => this.setState({ err }));
+        .catch((err) => this.setState({ err }));
     else {
       api
         .patchVotesByArticleId(article_id, inc_votes)
-        .catch(err => this.setState({ err }));
+        .catch((err) => this.setState({ err }));
     }
   };
 
   render() {
     const { votes, loggedUser } = this.props;
-
     const { voteChange, err } = this.state;
     if (err) {
       return <ErrorPage err={err} />;
@@ -37,6 +36,7 @@ export default class Voting extends Component {
       return (
         <section>
           <p className="voting-title">Votes: {votes + voteChange}</p>
+
           <button
             className="votes-btn"
             onClick={() => this.handleClick(1)}
