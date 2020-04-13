b641b399c84be29a420735cdee937d1e058f2119
ikraP <ikrapervaiz@gmail.com>
2020-04-13 15:27:39 +0100

    increasing click area for latest news article

--- src/components/LatestNewsArticles.jsx
+++ src/components/LatestNewsArticles.jsx
@@ -5,19 +5,19 @@ import { FaRegComment } from "react-icons/fa";
 export default function LatestNewsArticles(props) {
   const { article } = props;
   return (
-    <article className="l-card">
-      <img className="l-card-img" src="" alt="" />
-      <p className="l-card-small-title">Featured</p>
-      <Link
-        to={`/articles/${article.article_id}`}
-        style={{ textDecoration: "none", color: "inherit" }}
-      >
+    <Link
+      to={`/articles/${article.article_id}`}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <article className="l-card">
+        <img className="l-card-img" src="" alt="" />
+        <p className="l-card-small-title">Featured</p>
         <p className="l-card-title">{article.title}</p>
         <p className="l-card-comments">
           <FaRegComment size={20} comments={article.comment_count} />{" "}
           {article.comment_count} comments
         </p>{" "}
-      </Link>
-    </article>
+      </article>
+    </Link>
   );
 }
